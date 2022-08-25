7eb2765472339ffffd37dd7eacca2db8ec776d16
sudhanshu <mauryasudhanshu930@gmail.com>
2022-08-25 22:23:33 +0530

    border completed

--- src/components/clockOne/Clock1.jsx
+++ src/components/clockOne/Clock1.jsx
@@ -8,7 +8,7 @@ function Clock1() {
   const [hour, sethour] = useState("");
   const [ampm, setampm] = useState("");
   const [day, setDay] = useState(2);
-
+  // const array = [0,1,2,3,4,5,6,7,8,9]
   // const date = new Date();
   // // sethour(date.getHours());
 
@@ -20,9 +20,12 @@ function Clock1() {
 
   useEffect(() => {
     const date = new Date();
+    var second = date.getSeconds();
     sethour(date.getHours());
     var dayno = date.getDay();
     setDay(dayno);
+    // document.getElementsByClassName("ccc")[0].style.transform = `rotate(${((second * 6) - 180)}deg)`;
+
 
 
     if (dayno == 1) {
@@ -52,6 +55,17 @@ function Clock1() {
       var hourr = currentTime.getHours();
       var minute = currentTime.getMinutes();
       var second = currentTime.getSeconds();
+     if(hourr ==0 || hourr==1 ||  hourr==2||hourr==3 || hourr==4 || hourr==5 || hourr==6 || hourr==7 || hourr==8 ||hourr==9){
+      hourr = "0"+hourr;
+      // hourr.concat(0)
+
+     }
+     if(second ==0 || second==1 ||  second==2||second==3 || second==4 || second==5 || second==6 || second== 7|| second==8 ||second==9){
+      second = "0"+second
+      // second.concate(0)
+
+     }
+      // if()
 
 
 
@@ -62,18 +76,36 @@ function Clock1() {
       document.getElementsByClassName("digitalHour")[0].innerHTML = hourr
       document.getElementsByClassName("digitalMinute")[0].innerHTML = minute
       document.getElementsByClassName("digitalSecond")[0].innerHTML = second
+      document.getElementsByClassName("ccc")[0].style.transform = `rotate(${((second * 6) - 180)}deg)`;
 
       // clearInterval(interval);
 
     }
 
     const interval = setInterval(rotatesecond, 1000);
+
+
+    //     const rotatesecondd =()=>{
+    //       var currentTime = new Date();
+    //       var second = currentTime.getSeconds();
+    //       document.getElementsByClassName("ccc")[0].style.transform = `rotate(${((second * 6) - 180)}deg)`;
+
+    //       // console.log("hello");
+    // // 
+
+
+    //     }
+    //     const interval2 = setInterval(rotatesecondd, 60000);
+
   }, [])
 
   return (
     <>
       <div className="clock1">
         <div className="clock1container">
+          <div className="ccc">
+          </div>
+
           <div className="clockOne">
             <div className="hourhand"></div>
             <div className="dot"><h1 id='demo'>.</h1></div>
@@ -85,25 +117,30 @@ function Clock1() {
 
 
         <div className="timeContainer">
-          <div className="amPm">
-            {hour > 12 ? (
-              <h1>PM</h1>
-            ) : (
-              <h1>AM</h1>
-            )}
-          </div>
-          <div className="timegroup">
+          <div className="component1">
 
-            <div className="time">
-              <h1 className='digitalHour'>00</h1>
-              <h1>:</h1>
-              <h1 className='digitalMinute'>00</h1>
-              <h1>:</h1>
-              <h1 className='digitalSecond'>00</h1>
+
+            <div className="amPm">
+              {hour > 12 ? (
+                <h1 className='am_pm'>PM</h1>
+              ) : (
+                <h1 className='am_pm'>AM</h1>
+              )}
             </div>
-            <div className="day">
-              <h1 className='digitalDay'></h1>
+            <div className="timegroup">
+
+              <div className="time">
+                <h1 className='digitalHour'>00</h1>
+                <h1 className='colan'>:</h1>
+                <h1 className='digitalMinute'>00</h1>
+                <h1 className='colan'>:</h1>
+                <h1 className='digitalSecond'>00</h1>
+              </div>
+
             </div>
+          </div>
+          <div className="day">
+            <h1 className='digitalDay'></h1>
           </div>
 
         </div>
